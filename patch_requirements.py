393f2c22f07f049fc019f9e992e7b0c1edb74ce0
PelleNybe <157639057+PelleNybe@users.noreply.github.com>
2026-09-01 20:19:31 +0000

    Fix CI backwards compatibility with Python 3.8-3.10

--- patch_requirements.py
+++
@@ -1,9 +0,0 @@
-import re
-
-with open("requirements.txt", "r") as f:
-    content = f.read()
-
-content = content.replace("pandas>=3.0.5", "pandas>=2.0.0")
-
-with open("requirements.txt", "w") as f:
-    f.write(content)
