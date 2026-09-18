7138b3e3d685ca5f77d3c3ebdc3d7b7f1086d3d8
PelleNybe <157639057+PelleNybe@users.noreply.github.com>
2026-09-18 21:20:57 +0000

    feat: implement Walk-Forward Optimization (WFO) engine

---
+++ crypto_momentum/__init__.py
@@ -0,0 +1,18 @@
+from .data_fetcher import DataFetcher
+from .indicators import MomentumIndicators, OnBalanceVolumeIndicator, MACD, SMAIndicator, BollingerBands, EMAIndicator, AverageTrueRange, IchimokuIndicator, VolumeWeightedAveragePrice, ADXIndicator, StochRSIIndicator
+from .signal_generator import SignalGenerator
+from .backtester import Backtester
+from .ai_predictor import AIPredictor
+from .mtf_utils import get_htf_interval, get_htf_period
+from .optimizer import WalkForwardOptimizer
+
+__all__ = [
+    'DataFetcher',
+    'MomentumIndicators',
+    'SignalGenerator',
+    'Backtester',
+    'AIPredictor',
+    'get_htf_interval',
+    'get_htf_period',
+    'WalkForwardOptimizer'
+]
